02f7dc74c9bde26108ae47e5930eb11c4bceca53
chocoboy08 <chocoboy0508@gmail.com>
2025-07-07 14:36:00 +0900

    refactor: production 환경에서만 init하도록 수정

--- apps/client/sentry.edge.config.ts
+++ apps/client/sentry.edge.config.ts
@@ -1,7 +1,8 @@
 import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  tracesSampleRate: 1,
-  debug: false,
-});
+process.env.NODE_ENV === "production" &&
+  Sentry.init({
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    tracesSampleRate: 1,
+    debug: false,
+  });
