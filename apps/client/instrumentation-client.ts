2bd5962a3f2b5b81b0297a6d639d80eb7a023de9
chocoboy08 <chocoboy0508@gmail.com>
2025-07-07 15:04:37 +0900

    refactor: init에 environment 옵션 추가

--- apps/client/instrumentation-client.ts
+++ apps/client/instrumentation-client.ts
@@ -3,6 +3,7 @@ import * as Sentry from "@sentry/nextjs";
 process.env.NODE_ENV === "production" &&
   Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    environment: process.env.NODE_ENV,
     integrations: [Sentry.replayIntegration()],
     tracesSampleRate: 1,
     replaysSessionSampleRate: 0.1,
